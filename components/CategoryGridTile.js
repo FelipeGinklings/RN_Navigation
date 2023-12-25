d80f8ede634ffabfa64b687b5ad430cb7d1c3fc8
FelipeGinklings <felipe.ginklings@gmail.com>
2023-12-24 22:27:59 -0300

    Displaying Items in a Grid

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Platform } from 'react-native';
 
-const CategoryGridTile = ({title, color}) => {
+const CategoryGridTile = ({ title, color }) => {
 	return (
-		<View>
-			<Pressable>
-				<View>
-					<Text>{title}</Text>
+		<View style={[styles.gridItem, { backgroundColor: color }]}>
+			<Pressable
+				android_ripple={{ color: '#ccc' }}
+				style={({ pressed }) => [
+					styles.button,
+					pressed ? styles.buttonPressed : null,
+				]}
+			>
+				<View style={styles.innerContainer}>
+					<Text style={styles.title}>{title}</Text>
 				</View>
 			</Pressable>
 		</View>
@@ -14,4 +20,36 @@ const CategoryGridTile = ({title, color}) => {
 
 export default CategoryGridTile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	gridItem: {
+		flex: 1,
+		margin: 16,
+		height: 150,
+		borderRadius: 8,
+		backgroundColor: 'white',
+		// ANDROID
+		elevation: 4,
+		// IOS
+		shadowColor: 'black',
+		shadowOpacity: 0.25,
+		shadowOffset: { width: 0, height: 0 },
+		shadowRadius: 8,
+		overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+	},
+	buttonPressed: {
+		opacity: 0.5,
+	},
+	button: {
+		flex: 1,
+	},
+	innerContainer: {
+		flex: 1,
+		padding: 16,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	title: {
+		fontWeight: 'bold',
+		fontSize: 16,
+	},
+});
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,7 +3,7 @@ import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
 const renderCategoryItem = (itemData) => {
-	return <CategoryGridTile title={itemData.item.title} color={itemData.item}/>
+	return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
 };
 
 const CategoriesScreen = () => {
@@ -12,6 +12,7 @@ const CategoriesScreen = () => {
 			data={CATEGORIES}
 			keyExtractor={(item) => item.id}
 			renderItem={renderCategoryItem}
+      numColumns={2}
 		></FlatList>
 	);
 };
