f1672dbd3eaf664d89caee5b27b14b257bbb5918
Felipe Ginklings <felipe.ginklings@gmail.com>
2023-12-31 17:28:35 -0300

    Getting Started with Redux & Redux Toolkit

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 // Icons
 import { Ionicons } from '@expo/vector-icons';
+// Redux
+import { Provider } from 'react-redux';
 
 // Navigators components
 import { NavigationContainer } from '@react-navigation/native';
@@ -13,7 +15,8 @@ import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
-import FavoritesContextProvider from './store/context/favorites-context';
+// import FavoritesContextProvider from './store/context/favorites-context';
+import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -61,7 +64,8 @@ const App = () => {
 	return (
 		<>
 			<StatusBar style="light" />
-			<FavoritesContextProvider>
+			{/* <FavoritesContextProvider> */}
+			<Provider store={store}>
 				<NavigationContainer>
 					<Stack.Navigator
 						screenOptions={{
@@ -100,7 +104,8 @@ const App = () => {
 						/>
 					</Stack.Navigator>
 				</NavigationContainer>
-			</FavoritesContextProvider>
+			</Provider>
+			{/* </FavoritesContextProvider> */}
 		</>
 	);
 };
